9ae68056364401de6924588a6b70b90473120f6b
KingWJC <15110038963@163.com>
2021-12-22 17:32:42 +0800

    system 04 heap

---
+++ code/src/main/java/com/example/system/class04/C03_HeapSort.java
@@ -0,0 +1,103 @@
+/**
+ * 堆排序
+ * 额外空间复杂度O(1)
+ */
+package com.example.system.class04;
+
+import java.util.Arrays;
+
+import com.example.utility.helper.ArrayTestHelper;
+
+public class C03_HeapSort {
+    /**
+     * 从上向下生成堆
+     */
+    public static void heapSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        // O(N*logN)
+        for (int i = 0; i < arr.length; i++) { // O(N)
+            heapInsert(arr, i); // O(LogN)
+        }
+
+        // O(N*logN)
+        for (int i = arr.length - 1; i > 0; i--) {
+            swap(arr, 0, i);
+            heapify(arr, 0, i);
+        }
+    }
+
+    /**
+     * 从下向上生成堆
+     */
+    public static void heapSortNew(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        // O(N)
+        for (int i = arr.length - 1; i >= 0; i--) {
+            heapify(arr, i, arr.length);
+        }
+
+        int heapSize = arr.length;
+        swap(arr, 0, --heapSize);
+
+        // O(N*logN)
+        while (heapSize > 0) {// O(N)
+            heapify(arr, 0, heapSize);// O(logN)
+            swap(arr, 0, --heapSize);// O(1)
+        }
+    }
+
+    /**
+     * arr[index]刚来的数，往上
+     */
+    private static void heapInsert(int[] arr, int index) {
+        while (arr[index] > arr[(index - 1) / 2]) {
+            swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    /**
+     * arr[index]位置的数，能否往下移动
+     * 为什么left 和 left+1 不能是等于 heapSize。
+     */
+    private static void heapify(int[] arr, int index, int heapSize) {
+        int left = index * 2 + 1;
+        while (left < heapSize) { // 下方还有孩子的时候
+            // 两个孩子中，谁的值大，把下标给largest
+            // 1）只有左孩子，left -> largest
+            // 2) 同时有左孩子和右孩子，右孩子的值<= 左孩子的值，left -> largest
+            // 3) 同时有左孩子和右孩子并且右孩子的值> 左孩子的值， right -> largest
+            int largest = left + 1 < heapSize && arr[left] < arr[left + 1] ? left + 1 : left;
+            // 父和较大的孩子之间，谁的值大，把下标给largest
+            largest = arr[index] < arr[largest] ? largest : index;
+            if (largest == index)
+                break;
+            swap(arr, index, largest);
+            index = largest;
+            left = index * 2 + 1;
+        }
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    /**
+     * 测试排序
+     */
+    public static void testSort(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    public static void main(String[] args) {
+        ArrayTestHelper.testSortMethod(C03_HeapSort::testSort, C03_HeapSort::heapSortNew);
+    }
+}
---
+++ code/src/main/java/com/example/system/class04/C04_SortArrayDistLessK.java
@@ -0,0 +1,102 @@
+/**
+ * 排序一个几乎有序的数组。
+ * 几乎有序是指，如果把数组排好顺序的话，
+ * 每个元素移动的距离一定不超过k，并且k相对于数组长度来说是比较小的
+ */
+package com.example.system.class04;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+import com.example.utility.helper.ArrayTestHelper;
+
+public class C04_SortArrayDistLessK {
+    public static void sortedArrDistanceLessK(int[] arr, int k) {
+        if (k == 0 || arr == null || arr.length < 2)
+            return;
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        int index = 0;
+        for (; index < Math.min(arr.length, k); index++) {
+            heap.add(arr[index]);
+        }
+
+        int i = 0;
+        for (; index < arr.length; i++, index++) {
+            arr[i] = heap.poll();
+            heap.add(arr[index]);
+        }
+
+        while (!heap.isEmpty()) {
+            arr[i++] = heap.poll();
+        }
+
+        // 不能使用K进行记录，有可能K比arr.Length大
+        // int index = 0;
+        // while (!heap.isEmpty()) {
+        // arr[index++] = heap.poll();
+        // if (index + k < arr.length) {
+        // heap.add(arr[index + k]);
+        // }
+        // }
+    }
+
+    public static void testSort(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    /**
+     * 生成一个几乎有序的数组，移动范围不超过K
+     */
+    public static int[] randomArrayNoMoveMoreK(int maxSize, int maxValue, int K) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        Arrays.sort(arr);
+
+        // 然后开始随意交换，但是保证每个数距离不超过K
+        // swap[i] == true, 表示i位置已经参与过交换
+        // swap[i] == false, 表示i位置没有参与过交换
+        boolean[] isSwap = new boolean[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            // 获取K范围内的需要交换的随机索引
+            int swapIndex = (int) (Math.random() * (K + 1));
+            swapIndex = Math.min(swapIndex, arr.length - 1);
+            if (!isSwap[i] && !isSwap[swapIndex]) {
+                isSwap[i] = true;
+                isSwap[swapIndex] = true;
+                swap(arr, i, swapIndex);
+            }
+        }
+        return arr;
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 10;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int k = (int) (Math.random() * maxSize) + 1;
+            int[] arr1 = randomArrayNoMoveMoreK(maxSize, maxValue, k);
+            int[] arr2 = ArrayTestHelper.copyArray(arr1);
+            // System.out.println(Arrays.toString(arr1));
+            sortedArrDistanceLessK(arr1, k);
+            testSort(arr2);
+            if (!ArrayTestHelper.isEquals(arr1, arr2)) {
+                succeed = false;
+                System.out.println(Arrays.toString(arr1));
+                System.out.println(Arrays.toString(arr2));
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+    }
+}
