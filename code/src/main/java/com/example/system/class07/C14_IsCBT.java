159ff660b89ca6d2d2caa15625c819b60e0840f2
KingWJC <15110038963@163.com>
2022-01-06 17:26:49 +0800

    system07 binary tree CBS maxSubBSTHead

--- code/src/main/java/com/example/system/class07/C14_IsCBT.java
+++ code/src/main/java/com/example/system/class07/C14_IsCBT.java
@@ -3,13 +3,23 @@
  */
 package com.example.system.class07;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import com.example.utility.entity.BinaryTreeNode;
+import com.example.utility.helper.TreeNodeTestHelper;
 
 public class C14_IsCBT {
-    public static boolean isComplateBT(BinaryTreeNode head) {
+    /**
+     * 递归后序
+     */
+    public static boolean isCompleteBT(BinaryTreeNode head) {
         return process(head).isCBT;
     }
 
+    /**
+     * 对每一棵子树, 是否满二叉树，是否为完全二叉树，子数的高度
+     */
     public static class Info {
         boolean isFull;
         boolean isCBT;
@@ -24,6 +34,7 @@ public class C14_IsCBT {
 
     private static Info process(BinaryTreeNode node) {
         if (node == null) {
+            //空信息好设置
             return new Info(0, true, true);
         }
 
@@ -33,16 +44,77 @@ public class C14_IsCBT {
         int height = Math.max(left.height, right.height) + 1;
         boolean isFull = left.isFull && right.isFull && right.height == left.height;
         boolean isCBT = false;
-        if (left.isFull && right.isFull && left.height == right.height) {
-            isCBT = true;
-        } else if (left.isFull && right.isFull && left.height == right.height + 1) {
-            isCBT = true;
-        } else if (left.isFull && right.isCBT && left.height == right.height) {
-            isCBT = true;
-        } else if (left.isCBT && right.isFull && left.height == right.height + 1) {
-            isCBT = true;
+        // 4中可能的情况
+        if (left.height == right.height) {
+            if (left.isFull && right.isFull)
+                isCBT = true;
+            else if (left.isFull && right.isCBT)
+                isCBT = true;
+        } else if (left.height == right.height + 1) {
+            if (left.isFull && right.isFull)
+                isCBT = true;
+            else if (left.isCBT && right.isFull)
+                isCBT = true;
         }
 
         return new Info(height, isFull, isCBT);
+    }
+
+    /**
+     * 对数器
+     * 层级遍历树，根据上一个节点是否只有一个子节点，来判断是否是完整二叉树
+     */
+    public static boolean isCompleteBTUseQueue(BinaryTreeNode head) {
+        if (head == null)
+            return true;
+
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+        queue.add(head);
+        BinaryTreeNode left;
+        BinaryTreeNode right;
+        // 是否遇到过左右两个孩子不双全的节点
+        boolean isLeaf = false;
+        while (!queue.isEmpty()) {
+            BinaryTreeNode node = queue.poll();
+            left = node.left;
+            right = node.right;
+
+            if (left == null && right != null)
+                return false;
+            // 如果遇到了不双全的节点之后，又发现当前节点不是叶节点
+            if (isLeaf && (left != null || right != null))
+                return false;
+
+            if (left != null) {
+                queue.add(left);
+            }
+            if (right != null) {
+                queue.add(right);
+            }
+
+            if (left == null || right == null) {
+                isLeaf = true;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int maxValue = 100;
+        int maxLevel = 5;
+        int testTimes = 100000;
+        System.out.println("start test");
+        for (int i = 0; i < testTimes; i++) {
+            BinaryTreeNode head = TreeNodeTestHelper.generateRandomBTS(maxLevel, maxValue);
+            boolean flag1 = isCompleteBT(head);
+            boolean flag2 = isCompleteBTUseQueue(head);
+            if (flag1 != flag2) {
+                TreeNodeTestHelper.printTree(head);
+                System.out.println(flag1 + " error " + flag2);
+                break;
+            }
+        }
+        System.out.println("finished");
     }
 }
---
+++ code/src/main/java/com/example/system/class07/C15_MaxSubBSTHead.java
@@ -0,0 +1,156 @@
+/**
+ * 二叉树中最大的二叉搜索子树的头节点
+ */
+package com.example.system.class07;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.utility.entity.BinaryTreeNode;
+import com.example.utility.helper.TreeNodeTestHelper;
+
+public class C15_MaxSubBSTHead {
+    /**
+     * 递归后序
+     */
+    public static BinaryTreeNode maxSubBSTHead(BinaryTreeNode head) {
+        if (head == null)
+            return null;
+        return process(head).maxSubBSTHead;
+    }
+
+    public static class Info {
+        int min;
+        int max;
+        int maxSubBSTSize;
+        BinaryTreeNode maxSubBSTHead;
+
+        public Info(int min, int max, int maxSubBSTSize, BinaryTreeNode maxSubBSTHead) {
+            this.min = min;
+            this.max = max;
+            this.maxSubBSTHead = maxSubBSTHead;
+            this.maxSubBSTSize = maxSubBSTSize;
+        }
+    }
+
+    /**
+     * 三种情况
+     */
+    private static Info process(BinaryTreeNode node) {
+        if (node == null) {
+            return null;
+        }
+
+        Info left = process(node.left);
+        Info right = process(node.right);
+
+        int max = node.value;
+        int min = node.value;
+        int maxSubBSTSize = -1;
+        BinaryTreeNode maxSubBSTHead = null;
+        // 左子树有最大二叉搜索树
+        if (left != null) {
+            max = Math.max(left.max, max);
+            min = Math.min(left.min, min);
+            maxSubBSTSize = left.maxSubBSTSize;
+            maxSubBSTHead = left.maxSubBSTHead;
+        }
+        // 右子树有最大二叉搜索树
+        if (right != null) {
+            max = Math.max(right.max, max);
+            min = Math.min(right.min, min);
+            if (right.maxSubBSTSize > maxSubBSTSize) {
+                maxSubBSTSize = right.maxSubBSTSize;
+                maxSubBSTHead = right.maxSubBSTHead;
+            }
+        }
+
+        // 当前树是二叉搜索树，其左右子树都是二叉搜索树
+        boolean leftIsBST = left == null || left.maxSubBSTHead == node.left;
+        boolean rightIsBST = right == null || right.maxSubBSTHead == node.right;
+        if (leftIsBST && rightIsBST) {
+            boolean leftMaxLessX = left == null || left.max < node.value;
+            boolean rightMinMoreX = right == null || right.min > node.value;
+            if (leftMaxLessX && rightMinMoreX) {
+                int leftSize = left == null ? 0 : left.maxSubBSTSize;
+                int rightSize = right == null ? 0 : right.maxSubBSTSize;
+                maxSubBSTSize = leftSize + rightSize + 1;
+                maxSubBSTHead = node;
+            }
+        }
+        return new Info(min, max, maxSubBSTSize, maxSubBSTHead);
+    }
+
+    /**
+     * 对数器
+     * 暴力层级遍历，从上到下，第一个找到的二叉搜索树，就是最大二叉搜素树
+     */
+    public static BinaryTreeNode maxSubBSTHeadTest(BinaryTreeNode head) {
+        if (head == null)
+            return null;
+
+        int maxSubBSTSize = getBSTSize(head);
+        if (maxSubBSTSize > 0) {
+            return head;
+        }
+
+        BinaryTreeNode leftNode = maxSubBSTHeadTest(head.left);
+        BinaryTreeNode rightNode = maxSubBSTHeadTest(head.right);
+        int leftSize = getBSTSize(leftNode);
+        int rightSize = getBSTSize(rightNode);
+        if (leftSize > rightSize) {
+            return leftNode;
+        } else {
+            return rightNode;
+        }
+    }
+
+    private static int getBSTSize(BinaryTreeNode head) {
+        List<BinaryTreeNode> arr = new ArrayList<>();
+        inorder(head, arr);
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i).value <= arr.get(i - 1).value)
+                return 0;
+        }
+        return arr.size();
+    }
+
+    private static void inorder(BinaryTreeNode head, List<BinaryTreeNode> arr) {
+        if (head == null)
+            return;
+
+        inorder(head.left, arr);
+        arr.add(head);
+        inorder(head.right, arr);
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeNode head = new BinaryTreeNode(2);
+        head.left = new BinaryTreeNode(1);
+        head.right = new BinaryTreeNode(3);
+
+        BinaryTreeNode node1 = maxSubBSTHead(head);
+        BinaryTreeNode node2 = maxSubBSTHeadTest(head);
+
+            TreeNodeTestHelper.printTree(head);
+            System.out.println(node1.value + " error " + node2.value);
+
+
+        // int maxLevel = 5;
+        // int maxValue = 100;
+        // int testTimes = 100000;
+        // System.out.println("start test");
+        // for (int i = 0; i < testTimes; i++) {
+        // BinaryTreeNode head = TreeNodeTestHelper.generateRandomBTS(maxLevel,
+        // maxValue);
+        // BinaryTreeNode node1 = maxSubBSTHead(head);
+        // BinaryTreeNode node2 = maxSubBSTHeadTest(head);
+        // if (node1 != node2) {
+        // TreeNodeTestHelper.printTree(head);
+        // System.out.println(node1.value + " error " + node2.value);
+        // break;
+        // }
+        // }
+        // System.out.println("finished");
+    }
+}
